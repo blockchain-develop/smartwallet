21767aa35c7845c43f127fc1a095e2a66510420e
root <chain_dev@163.com>
2024-11-19 09:11:36 +0000

    update

--- tests/create_wallet_test.ts
+++ tests/create_wallet_test.ts
@@ -1,9 +1,17 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Smartwallet } from "../target/types/smartwallet";
+const { Keypair, ConfirmOptions } = require("@solana/web3.js");
 
 async function main() {
-  const provider = anchor.AnchorProvider.env();
+  const url = anchor.web3.clusterApiUrl("devnet");
+  console.log("url: ", url);
+  const connection = new anchor.web3.Connection(url, "confirmed");
+  let seed = Uint8Array.from([234, 212, 57, 137, 35, 17, 232, 92, 120, 194, 143, 102, 216, 107, 48, 229, 155, 18, 246, 139, 38, 132, 236, 183, 25, 228, 86, 109, 124, 165, 211, 146, 23, 45, 150, 153, 8, 189, 1, 79, 184, 11, 125, 231, 105, 235, 59, 26, 41, 206, 213, 116, 40, 183, 164, 59, 78, 149, 216, 16, 213, 29, 179, 178]);
+  let account = Keypair.fromSeed(seed.subarray(0, 32));
+  const wallet = new anchor.Wallet(account);
+  console.log("wallet address: ", wallet.publicKey.toBase58());
+  const provider = new anchor.AnchorProvider(connection, wallet, {});
   anchor.setProvider(provider);
   const program = anchor.workspace.Smartwallet as Program<Smartwallet>;
 
