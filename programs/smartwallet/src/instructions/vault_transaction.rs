82aabee565c5a7922d140a068e07be0292f3b939
root <chain_dev@163.com>
2024-11-25 07:12:24 +0000

    update

--- programs/smartwallet/src/instructions/vault_transaction.rs
+++ programs/smartwallet/src/instructions/vault_transaction.rs
@@ -9,11 +9,17 @@ use solana_program::pubkey::Pubkey;
 use solana_program::secp256k1_recover;
 
 #[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct InstructionArgs {
+    pub account_size: u8,
+    pub data: Vec<u8>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
 pub struct VaultTransactionArgs {
     pub owner: Vec<u8>,
     pub signs: Vec<u8>,
     pub recovery_id: u8,
-    pub data: Vec<u8>,
+    pub instructions: Vec<InstructionArgs>,
 }
 
 #[derive(Accounts)]
@@ -29,9 +35,6 @@ pub struct VaultTransaction<'info> {
     /// The creator of the multisig.
     #[account(mut)]
     pub payer: Signer<'info>,
-
-    /// CHECK:
-    pub program: AccountInfo<'info>,
 }
 
 impl VaultTransaction<'_> {
@@ -50,14 +53,20 @@ impl VaultTransaction<'_> {
         let args_signs = to_hex_string(&args.signs);
         msg!("parameter signs: {}", args_signs);
 
-        let args_data = to_hex_string(&args.data);
-        msg!("parameter data: {}", args_data);
+        for (i, instruction) in args.instructions.iter().enumerate() {
+            msg!("instrunction number: {}", i);
+
+            msg!("instrunction account size: {}", instruction.account_size);
+
+            let instruction_data = to_hex_string(&instruction.data);
+            msg!("instrunction data: {}", instruction_data);
+        }
 
         // todo
         // the data of sign
         // replay - transaction_index
 
-        // try to build the transaction
+        // the smart wallet owner address
         //
         let seeds_owner = args.owner.as_slice();
         msg!("{}", seeds_owner.len());
@@ -75,39 +84,60 @@ impl VaultTransaction<'_> {
                 return Err(WalletError::CannotCreateProgramAddress.into());
             }
         };
-        let mut instruction_accounts = vec![];
-        let mut instruction_account_infos = vec![];
-        for account_info in ctx.remaining_accounts.iter() {
-            if *account_info.key == smart_wallet_owner_address {
-                instruction_accounts.push(AccountMeta {
-                    pubkey: *account_info.key,
-                    is_signer: true,
-                    is_writable: account_info.is_writable,
-                });
-            } else {
-                instruction_accounts.push(AccountMeta {
-                    pubkey: *account_info.key,
-                    is_signer: account_info.is_signer,
-                    is_writable: account_info.is_writable,
-                });
+
+        // try to rebuild all instructions
+        //
+        let mut insturctions = vec![];
+        let mut instructions_accounts = vec![];
+        let mut raw_instructions = vec![];
+        let mut account_index = 0;
+        for instruction in args.instructions.iter() {
+            let mut instruction_accounts = vec![];
+            let mut instruction_account_infos = vec![];
+            let program_id = *ctx.remaining_accounts.get(account_index).unwrap().key;
+            account_index += 1;
+            for _i in 0..instruction.account_size {
+                let account_info = ctx.remaining_accounts.get(account_index).unwrap();
+                if *account_info.key == smart_wallet_owner_address {
+                    instruction_accounts.push(AccountMeta {
+                        pubkey: *account_info.key,
+                        is_signer: true,
+                        is_writable: account_info.is_writable,
+                    });
+                } else {
+                    instruction_accounts.push(AccountMeta {
+                        pubkey: *account_info.key,
+                        is_signer: account_info.is_signer,
+                        is_writable: account_info.is_writable,
+                    });
+                }
+                instruction_account_infos.push(account_info.clone());
+                account_index += 1;
             }
-            instruction_account_infos.push(account_info.clone());
+            let instruction = Instruction {
+                program_id: program_id,
+                accounts: instruction_accounts,
+                data: instruction.data.clone(),
+            };
+            let instruction_raw = bincode::serialize(&instruction).unwrap();
+            //
+            insturctions.push(instruction);
+            raw_instructions.push(instruction_raw);
+            instructions_accounts.push(instruction_account_infos);
         }
-        let instruction = Instruction {
-            program_id: *ctx.accounts.program.key,
-            accounts: instruction_accounts,
-            data: args.data,
-        };
-        let mut instruction_raw = bincode::serialize(&instruction).unwrap();
         let mut transaction_index = ctx.accounts.wallet.transaction_index.try_to_vec()?;
-        instruction_raw.append(&mut transaction_index);
-        msg!("raw instruction: {}", to_hex_string(&instruction_raw));
+        let mut raw = vec![];
+        for raw_instruction in raw_instructions.iter_mut() {
+            raw.append(raw_instruction);
+        }
+        raw.append(&mut transaction_index);
+        msg!("raw instruction: {}", to_hex_string(&raw));
 
         // verify the sec256k1 signature
         //
         let message_hash = {
             let mut hasher = solana_program::keccak::Hasher::default();
-            hasher.hash(&instruction_raw.as_slice());
+            hasher.hash(&raw.as_slice());
             hasher.result()
         };
         msg!("msg hash: {}", to_hex_string(&message_hash.try_to_vec()?));
@@ -133,7 +163,11 @@ impl VaultTransaction<'_> {
 
         // execute this instruction
         //
-        invoke_signed(&instruction, &instruction_account_infos[..], &[seeds])?;
+        for i in 0..raw_instructions.len() {
+            let instruction = insturctions.get(i).unwrap();
+            let instruction_account_infos = instructions_accounts.get(i).unwrap();
+            invoke_signed(&instruction, &instruction_account_infos[..], &[seeds])?;
+        }
 
         // nonce
         let wallet = &mut ctx.accounts.wallet;
---
+++ tests/transaction_multiple_transfer_sol.ts
@@ -0,0 +1,175 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Smartwallet } from "./../target/types/smartwallet";
+import idl from "./../target/idl/smartwallet.json";
+import { toBigInt } from "ethers";
+const { Keypair, ConfirmOptions, PublicKey } = require("@solana/web3.js");
+const secp256k1 = require('secp256k1');
+const { ethers, utils } = require("ethers");
+
+async function main() {
+  // solana provider & wallet
+  const url = anchor.web3.clusterApiUrl("devnet");
+  console.log("solana url: ", url);
+  const connection = new anchor.web3.Connection(url, "confirmed");
+  let secretKey = Uint8Array.from([234, 212, 57, 137, 35, 17, 232, 92, 120, 194, 143, 102, 216, 107, 48, 229, 155, 18, 246, 139, 38, 132, 236, 183, 25, 228, 86, 109, 124, 165, 211, 146, 23, 45, 150, 153, 8, 189, 1, 79, 184, 11, 125, 231, 105, 235, 59, 26, 41, 206, 213, 116, 40, 183, 164, 59, 78, 149, 216, 16, 213, 29, 179, 178]);
+  let keypair = Keypair.fromSecretKey(secretKey);
+  const wallet = new anchor.Wallet(keypair);
+  console.log("solana payer address: ", wallet.publicKey.toBase58());
+  const privKeyHex = Buffer.from(wallet.payer.secretKey).toString('hex')
+  const pubKeyHex = Buffer.from(wallet.publicKey.toBytes()).toString('hex')
+  console.log('solana payer public key: ', pubKeyHex)
+  console.log('solana payer private key: ', privKeyHex)
+  const provider = new anchor.AnchorProvider(connection, wallet, {});
+  anchor.setProvider(provider);
+
+  // program
+  const programAddress = new PublicKey('C7iFDZry7Kdg7gc829qBCPEPKKdDkf1gRMxNxDEqeYUX');
+  let program = anchor.workspace.Smartwallet as Program<Smartwallet>;
+
+  const owner = Buffer.from('024e1e9e0de1a665cbbcaaa4c3e9388e5900adc9ff5b9676f3b973dbe8b2b3aa', 'hex');
+  let [configAddress, configBump] = await PublicKey.findProgramAddress(
+    ['wallet', 'config', owner],
+    programAddress,
+  );
+  console.log('smart wallet config address: ', configAddress.toBase58())
+  console.log('smart wallet config bump: ', configBump)
+
+  let [ownerAddress, ownerBump] = await PublicKey.findProgramAddress(
+    ['wallet', 'owner', owner],
+    programAddress,
+  );
+  console.log('smart wallet owner address: ', ownerAddress.toBase58())
+  console.log('smart wallet owner bump: ', ownerBump)
+
+  // executed instruction
+  //
+  const receipt = new PublicKey('2ZUhLpkBwxvZakU5gA7J3pMLnPhfyZZGQY9ArJtFPLqw');
+  const transferInstuction = anchor.web3.SystemProgram.transfer({
+    fromPubkey: ownerAddress,
+    toPubkey: receipt,
+    lamports: 1000000,
+  })
+  transferInstuction.keys[1].isSigner = true;
+  let raw0 = encodeInstruction(transferInstuction);
+
+  // twice
+  let raw1 = Buffer.from(raw0);
+
+  // transaction index
+  let indexBuffer = Buffer.alloc(8);
+  indexBuffer.writeBigInt64LE(toBigInt(1));
+
+  //
+  let raw = Buffer.concat([raw0, raw1, indexBuffer]);
+  console.log('raw: ', raw.toString('hex'));
+
+  // sign the instruction raw
+  const privKey = Buffer.from('6c35d4230c67d604a129d5b9de1cd2667096d93d0b0ec01512d21d1a30fe7676', 'hex');
+  const pubKey = Buffer.from('03024e1e9e0de1a665cbbcaaa4c3e9388e5900adc9ff5b9676f3b973dbe8b2b3aa', 'hex');
+  const messageHash = ethers.keccak256(raw).slice(2, 66);
+  console.log('message hash: ', messageHash);
+  const messageHashBytes = Buffer.from(messageHash, 'hex');
+  const signObj = secp256k1.ecdsaSign(messageHashBytes, privKey);
+  console.log('verify the signature: ', secp256k1.ecdsaVerify(signObj.signature, messageHashBytes, pubKey));
+
+  // transaction
+  const tx = await program.methods
+    .executeTransaction({
+      owner: owner,
+      signs: Buffer.from(signObj.signature),
+      recoveryId: signObj.recoveryId,
+      instructions: [
+        {
+          accountSize: 2,
+          data: transferInstuction.data,
+        },
+        {
+          accountSize: 2,
+          data: transferInstuction.data,
+        }
+      ],
+    })
+    .accounts({
+      wallet: configAddress,
+      payer: wallet.publicKey,
+    })
+    .remainingAccounts(
+      [
+        {
+          isSigner: false,
+          isWritable: false,
+          pubkey: anchor.web3.SystemProgram.programId,
+        },
+        {
+          isSigner: false,
+          isWritable: true,
+          pubkey: ownerAddress,
+        },
+        {
+          isSigner: false,
+          isWritable: true,
+          pubkey: receipt,
+        },
+        {
+          isSigner: false,
+          isWritable: false,
+          pubkey: anchor.web3.SystemProgram.programId,
+        },
+        {
+          isSigner: false,
+          isWritable: true,
+          pubkey: ownerAddress,
+        },
+        {
+          isSigner: false,
+          isWritable: true,
+          pubkey: receipt,
+        }
+      ]
+    )
+    .signers([wallet.payer])
+    .preInstructions([
+      anchor.web3.ComputeBudgetProgram.setComputeUnitLimit({
+        units: 600000,
+      })
+    ])
+    .rpc();
+  console.log("transaction signature", tx);
+}
+
+function encodeInstruction(a: anchor.web3.TransactionInstruction) {
+  console.log("program key: ", a.programId.toBuffer().toString('hex'));
+  console.log("data: ", a.data.toString('hex'));
+  var i: number;
+  for (i = 0; i < a.keys.length; i++) {
+    const key = a.keys[i];
+    console.log("number: ", i)
+    console.log("is signer: ", key.isSigner);
+    console.log("is writable: ", key.isWritable);
+    console.log("public key: ", key.pubkey);
+  }
+  //
+  const programIdBuffer = Buffer.alloc(32).fill(a.programId.toBuffer());
+  let keysBuffer = Buffer.alloc(8);
+  keysBuffer.writeBigInt64LE(toBigInt(a.keys.length));
+  for (i = 0; i < a.keys.length; i++) {
+    const key = a.keys[i];
+    let keyBuffer = Buffer.alloc(34).fill(key.pubkey.toBuffer());
+    keyBuffer.writeInt8(key.isSigner ? 1 : 0, 32);
+    keyBuffer.writeInt8(key.isWritable ? 1 : 0, 33);
+    keysBuffer = Buffer.concat([keysBuffer, keyBuffer]);
+  }
+  let dataBuffer = Buffer.alloc(8 + a.data.length);
+  dataBuffer.writeBigInt64LE(toBigInt(a.data.length));
+  dataBuffer.fill(a.data, 8);
+
+  return Buffer.concat([programIdBuffer, keysBuffer, dataBuffer]);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
